6a5a35a47a200edf81713385c7b28c852684faf7
Eli <emmuzio0510@icloud.com>
2023-07-07 16:13:27 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+
+const header = document.querySelector('h1#victory');
+
+
+if (header) {
+  header.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = "${Elijah} is the champion"; 
+
+
+document.body.appendChild(newHeader); 
